a638811e33a314e12b38a931d9ca651615a9d32a
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-21 17:04:20 +0200

    socket.io heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
-    origins: `localhost:8080 192.168.50.155:8080 ${process.env.PORT}`
+    origins: `localhost:8080 192.168.50.155:8080 https://dashboard.heroku.com/apps/aweigl-social-network:*`
 });
 const compression = require("compression");
 const bodyParser = require("body-parser");
